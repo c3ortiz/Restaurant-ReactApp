2bab588da22b08c9376b4c94fbcf7da0870aacfe
Cesar Ortiz <ceortiz@bancolombia.com.co>
2023-02-01 09:28:17 -0500

    Finished app

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -8,7 +8,7 @@ const FindUs = () => (
       <SubHeading title='Contact' />
       <h1 className='headtext__cormorant' style={{ marginBottom: '3rem'}}>Find us</h1>
       <div className='app__wrapper-content'>
-        <p className='p__opensans'>Here goes your business adress</p>
+        <p className='p__opensans'>9 W 53rd St, New York, NY 10019, USA</p>
         <p className='p__cormorant' style={{ color: '#DCCA87', margin: '2rem 0'}}>Opening hours</p>
         <p className='p__opensans'>Mon - Fri: 12:00 am - 11:00 pm</p>
         <p className='p__opensans'>Sat - Sun: 1:00 pm - 1:00 am</p>
